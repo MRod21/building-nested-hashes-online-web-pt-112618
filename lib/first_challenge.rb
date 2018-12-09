2358722a3add026951890b8b6ae6dfc68818485d
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2018-12-09 18:38:01 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,13 @@
 def first_challenge
    epic_tragedy = {
+<<<<<<< HEAD
      :montague => {},
      :capulet => {}
    }
+=======
+     montague: = {},
+     capulet: = {}
+   }
+ end
+>>>>>>> 9ac0932409028f48b77b4ecc1779e2d28955d3e1
 end
